a8fa015505ba272796796cd1ef8b77cab5e2a3f6
Andrei Vulpe <andreivulpe02@yahoo.com>
2022-08-12 10:37:01 +0300

    Added comments

--- ElectroShop/src/main/java/Service/ProductService.java
+++ ElectroShop/src/main/java/Service/ProductService.java
@@ -16,12 +16,14 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
 
+    // Method to get all undeleted products
     public List<Product> getUndeletedProducts() {
         List<Product> tmp = productRepository.findAll();
         tmp.removeIf(product -> !product.getDeleted());
         return tmp;
     }
 
+    // Method to get all undeleted productsDTO
     public List<ProductDTO> getUndeletedProductsDTO() {
         return productRepository.findAll().stream()
                 .filter(x -> !x.getDeleted())
@@ -29,26 +31,27 @@ public class ProductService {
                 .collect(Collectors.toList());
     }
 
+    // Method to get all products, even those deleted
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
 
+    // Method to get all productsDTO, even those deleted
     public List<ProductDTO> getAllProductsDTO() {
         return productRepository.findAll().stream().map(productMapper::toProductDTO).collect(Collectors.toList());
     }
 
+    // Method to add a product
     public Product addProduct(Product product) {
         return productRepository.save(product);
     }
 
-    public ProductDTO addProductDTO(Product product) {
-        return productMapper.toProductDTO(productRepository.save(product));
-    }
-
+    // Method to delete a product by id
     public void deleteProduct(Integer id) {
         productRepository.findById(id).get().setDeleted(true);
     }
 
+    // Method to update a product stock (minus, plus)
     public Product updateProductStock(Integer id, String typeOfUpdate, Integer value) {
         Product tmp = productRepository.findById(id).get();
         int initialValue = tmp.getInitialStock().get();
@@ -61,13 +64,15 @@ public class ProductService {
         return productRepository.save(tmp);
     }
 
-    public Product incrementProduct(Integer id) {
+    // Method to increment a product stock
+    public Product incrementProductStock(Integer id) {
         Product tmp = productRepository.findById(id).get();
         tmp.getInitialStock().incrementAndGet();
         return productRepository.save(tmp);
     }
 
-    public Product decrementProduct(Integer id) {
+    // Method to decrement a product stock
+    public Product decrementProductStock(Integer id) {
         Product tmp = productRepository.findById(id).get();
         tmp.getInitialStock().decrementAndGet();
         return productRepository.save(tmp);
