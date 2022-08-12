5b0236ef3d1e0eb9cf6aa73f930c174656c2dd28
Andrei Vulpe <andreivulpe02@yahoo.com>
2022-08-12 10:34:38 +0300

    Added commments

--- ElectroShop/src/main/java/Controller/ProductController.java
+++ ElectroShop/src/main/java/Controller/ProductController.java
@@ -19,53 +19,57 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
+    // Method to get all undeleted products
     @GetMapping("all/undeleted")
     public List<Product> getUndeletedProducts() {
         return productService.getUndeletedProducts();
     }
 
+    // Method to get all undeleted productsDTO
     @GetMapping("dto/all/undeleted")
     public List<ProductDTO> getUndeletedProductsDTO() {
         return productService.getUndeletedProductsDTO();
     }
 
+    // Method to get all products, even those deleted
     @GetMapping("all")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
 
+    // Method to get all productsDTO, even those deleted
     @GetMapping("dto/all")
     public List<ProductDTO> getAllProductsDTO() {
         return productService.getAllProductsDTO();
     }
 
+    // Method to add a product
     @PostMapping("add")
     public Product addProduct(@RequestBody Product product) {
         return productService.addProduct(product);
     }
 
-    @PostMapping("dto/add")
-    public ProductDTO addProductDTO(@RequestBody Product product) {
-        return productService.addProductDTO(product);
-    }
-
+    // Method to delete a product by id
     @PutMapping("delete/{id}")
     public void deleteProduct(@PathVariable Integer id) {
         productService.deleteProduct(id);
     }
 
+    // Method to update a product stock (minus, plus)
     @PutMapping("update/{id}/{typeOfUpdate}/{value}")
     public Product updateProductStock(@PathVariable Integer id, @PathVariable String typeOfUpdate, @PathVariable Integer value) {
         return productService.updateProductStock(id, typeOfUpdate, value);
     }
 
+    // Method to increment a product stock
     @PutMapping("update/{id}/increment")
     public Product incrementProduct(@PathVariable Integer id) {
-        return productService.incrementProduct(id);
+        return productService.incrementProductStock(id);
     }
 
+    // Method to decrement a product stock
     @PutMapping("update/{id}/decrement")
     public Product decrementProduct(@PathVariable Integer id) {
-        return productService.decrementProduct(id);
+        return productService.decrementProductStock(id);
     }
 }
