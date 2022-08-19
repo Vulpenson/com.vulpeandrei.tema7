a10b4a6bcdfc4c6f767cf1932b78bfb3408de9df
Andrei Vulpe <andreivulpe02@yahoo.com>
2022-08-19 15:53:48 +0300

    ProductServiceTest completed

--- ElectroShop/src/test/java/com/example/electroshop/ProductServiceTest.java
+++ ElectroShop/src/test/java/com/example/electroshop/ProductServiceTest.java
@@ -4,12 +4,17 @@ import Model.Product;
 import Model.Type;
 import Repository.ProductRepository;
 import Service.ProductService;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class ProductServiceTest {
@@ -20,12 +25,18 @@ public class ProductServiceTest {
     @InjectMocks
     ProductService productService;
 
+    @Test
     public void it_should_increment_product_stock_count() {
-        Product testProduct = new Product(null,
+        Product testProduct = new Product(5,
                                             "Boxa portabila",
                                             new AtomicInteger(100),
                                             Type.ELC,
                                             false);
-
+        when(productRepository.save(any(Product.class))).thenReturn(new Product());
+        Product createdProduct = productService.addProduct(testProduct);
+        productService.incrementProductStock(testProduct.getId());
+        assertThat(createdProduct.getInitialStock().get()).isSameAs(testProduct.getInitialStock().get());
     }
+
+
 }
